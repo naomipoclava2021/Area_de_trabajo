e1c3926fe2eeecee389984beebf87b98b470c827
MartinPasqua <mart11@live.com.ar>
2022-06-13 13:27:54 -0300

    Agregue ubicación

--- Presentacion/src/SobreMi/Datos.java
+++ Presentacion/src/SobreMi/Datos.java
@@ -3,7 +3,7 @@ package SobreMi;
 public class Datos {
 	public static void main (String[] args)
 	{System.out.println ("Hola soy Martin Pasqua");
-		
+	 System.out.println ("Soy de zona norte");	
 		
 	}
 	
