a84d18d2e043a863f7d6f49d5287bb022dbe927f
MartinPasqua <mart11@live.com.ar>
2022-06-13 13:48:54 -0300

    Agregue gustos

--- Clase9-/src/Clase9/Clase9.java
+++
@@ -1,7 +0,0 @@
-package Clase9;
-
-public class Clase9 {
-//Metodo set modifica el valor, metodo get returna el valor
-
-	
-}
--- EjemploPractico/src/module-info.java
+++
@@ -1,6 +0,0 @@
-public class EjemploPractico {
-	public static void main (String[] args ) {
-String a=Martin;
-int b=23;
-System.out.println ("Nombre: " + a + "Edad" + b);
-	}}
