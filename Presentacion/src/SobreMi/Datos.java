9acc3c6e7abe43ed5bdb9c357ed10dd6aaaaa62f
Naomi Poclava <milagrito1dediciembre@gmail.com>
2022-06-13 16:47:06 -0300

    me presente en eclipse

--- Presentacion/src/SobreMi/Datos.java
+++ Presentacion/src/SobreMi/Datos.java
@@ -1,9 +1,11 @@
 package SobreMi;
 
 public class Datos {
-	public static void main (String[] args)
-	{System.out.println ("Hola soy Martin Pasqua");
-	 System.out.println ("Soy de zona norte");	
+	public static void main (String[] args){
+		System.out.println ("Hola soy Martin Pasqua");
+		System.out.println ("Soy de zona norte");
+		
+		System.out.println("Martin soy naomi, y le puse un poco de identacion a tu codigo");
 		
 	}
 	
---
+++ Presentacion/src/sobreMiNaomi/Datos.java
@@ -0,0 +1,13 @@
+package sobreMiNaomi;
+
+public class Datos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola soy naomi milagro Poclava");
+		System.out.println("Tengo 21 años");
+		System.out.println("soy de zona centro");
+
+	}
+
+}
